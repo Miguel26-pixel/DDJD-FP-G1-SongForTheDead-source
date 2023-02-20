fd33b0cc2573ee08343451d28d1700b53edf97f9
eukia <ritam.mendes@gmail.com>
2023-02-20 01:59:50 +0000

    Implement sprite flipping on movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public Vector2 _speed = new Vector2(10, 10);
     private Animator _animator;
+    private bool _facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,14 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(_speed.x * inputX, _speed.y * inputY, 0);
 
         _animator.SetBool("isWalking", movement.x != 0 || movement.y != 0);
+
+
+        if ((_facingRight && movement.x < 0) || (!_facingRight && movement.x >= 0))
+        {
+            GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+        }
+
+        _facingRight = movement.x >= 0;
 
         movement *= Time.deltaTime;
 
