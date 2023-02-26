1cd838cbfa1749f3fbe3444e547719fa4194cfc6
Miguel Amorim <migamorim2012@gmail.com>
2023-02-26 16:45:52 +0000

    Add minighost animations and follow hero

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIA : MonoBehaviour
+{
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+    public bool isAlive = true;
+    public bool isToAttack = false;
+
+    public bool shouldRotate;
+
+    public LayerMask whatIsPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+        anim.SetBool("isAlive", true);
+    }
+
+    private void Update()
+    {
+        anim.SetBool("isRunning", isInChaseRange);
+
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+
+        dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        dir.Normalize();
+        movement = dir;
+        if(shouldRotate)
+        {
+            anim.SetFloat("x", dir.x);
+            anim.SetFloat("y", dir.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isInChaseRange && !isInAttackRange)
+        {
+            anim.SetBool("isToAttack", false);
+            MoveCharacter(movement);
+        }
+        if(isInAttackRange)
+        {
+            anim.SetBool("isToAttack", true);
+        }
+    }
+
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir*speed*Time.deltaTime));
+    }
+}
