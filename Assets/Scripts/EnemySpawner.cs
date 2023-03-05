79577eb55964d1cbce3b6643c9f0a61bcb6055a2
Miguel Amorim <migamorim2012@gmail.com>
2023-03-05 11:16:52 +0000

    Save scene with new map

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,11 +9,14 @@ public class EnemySpawner : MonoBehaviour
     private GameObject uglyghostPrefab;
 
     [SerializeField]
-    private float minighostInterval = 1f;
+    private float minighostInterval = 6f;
     [SerializeField]
-    private float uglyghostInterval = 2f;
+    private float uglyghostInterval = 7f;
 
     private Transform target;
+    private Transform target2;
+    private Transform target3;
+
 
 
     void Start()
@@ -21,6 +24,8 @@ public class EnemySpawner : MonoBehaviour
         StartCoroutine(spawnEnemy(minighostInterval, minighostPrefab));
         StartCoroutine(spawnEnemy(uglyghostInterval, uglyghostPrefab));
         target = GameObject.FindWithTag("Obelisk").transform;
+        target2 = GameObject.FindWithTag("Obelisk1").transform;
+        target3 = GameObject.FindWithTag("Obelisk2").transform;
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
@@ -29,6 +34,10 @@ public class EnemySpawner : MonoBehaviour
         if (enemy != null)
         {
             GameObject newEnemy = Instantiate(enemy, new Vector3(target.position.x + Random.Range(-1f, 1), target.position.y - 4, 0), Quaternion.identity);
+            StartCoroutine(spawnEnemy(interval, enemy));
+            GameObject newEnemy2 = Instantiate(enemy, new Vector3(target2.position.x + Random.Range(-1f, 1), target2.position.y - 4, 0), Quaternion.identity);
+            StartCoroutine(spawnEnemy(interval, enemy));
+            GameObject newEnemy3 = Instantiate(enemy, new Vector3(target3.position.x + Random.Range(-1f, 1), target3.position.y - 4, 0), Quaternion.identity);
             StartCoroutine(spawnEnemy(interval, enemy));
         }
     }
